e8e3fd27a9283b20a282943a309aa06d12f786d3
Anusha <anushadhulipudi.ad@gmail.com>
2019-07-18 10:27:09 +0530

    Created a todo class as best practice instead of hardcoded objects

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.css']
 })
+
+
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    { id: 1, description: 'Learn to Paint'},
-    { id: 2, description: 'Practice Angular everyday'},
-    { id: 3, description: 'Visit Singapore'}
+    new Todo(1, 'Learn to Paint', false, new Date()),
+    new Todo(2, 'Practice Angular everyday and become an expert', false, new Date()),
+    new Todo(3, 'Visit India', false, new Date())
+    // { id: 1, description: 'Learn to Paint'},
+    // { id: 2, description: 'Practice Angular everyday'},
+    // { id: 3, description: 'Visit Singapore'}
   ]
 
   // todo = {
