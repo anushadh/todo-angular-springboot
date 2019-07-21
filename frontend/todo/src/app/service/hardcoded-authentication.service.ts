02c955a7662b0d4ae5528c2a71c6e60bc36597ce
Anusha <anushadhulipudi.ad@gmail.com>
2019-07-21 14:09:48 +0530

    Implemented authentication using session storage Added a service for authentication

---
+++ frontend/todo/src/app/service/hardcoded-authentication.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HardcodedAuthenticationService {
+
+  constructor() { }
+
+  authenticate(username, password) {
+    console.log('before: '+ this.isUserLoggedIn());
+    if(username === "anusha" && password === "1234") {
+      sessionStorage.setItem('authenticatedUser', username);
+      console.log('after: '+ this.isUserLoggedIn());
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null);
+  }
+}
