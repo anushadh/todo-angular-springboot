3ecba5a605369772cfcc377fbb6a5f79d82ce30a
Anusha <anushadhulipudi.ad@gmail.com>
2019-07-23 09:57:11 +0530

    Created a simple hello world bean and endpoint with path variable

--- restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/todo/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,17 +1,29 @@
 package com.todo.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	
+
 	// Two types @RequestMapping(method = RequestMethod.GET, path = "/helloworld")
-	@GetMapping(path = "/helloworld")
+	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World !";
 	}
 
+	// hello-world-bean
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World !");
+	}
+
+	// hello-world with path variable
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s !", name));
+	}
 }
